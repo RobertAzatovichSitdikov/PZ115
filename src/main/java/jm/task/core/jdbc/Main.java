e9ba709c50de06567f46cb174e7ae9067eeab17d
RobertAzatovichSitdikov <kotiknumbertwo@gmail.com>
2023-08-23 14:24:14 +0300

    v2.0

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,29 +1,26 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDao userDao = new UserDaoJDBCImpl();
+        UserService userService = new UserServiceImpl();
 
-        userDao.createUsersTable();
+        userService.createUsersTable();
 
-        userDao.saveUser("Заур","Гизатуллин", (byte) 25);
-        userDao.saveUser("Наиль","Толибеков", (byte) 22);
-        userDao.saveUser("Маска","Зорро", (byte) 70);
-        userDao.saveUser("Алибек","Алибеков", (byte) 35);
-        userDao.saveUser("Дана","Мансурова", (byte) 99);
+        userService.saveUser("Заур","Гизатуллин", (byte) 25);
+        userService.saveUser("Наиль","Толибеков", (byte) 22);
+        userService.saveUser("Маска","Зорро", (byte) 70);
+        userService.saveUser("Дана","Мансурова", (byte) 99);
 
-        userDao.removeUserById(3);
+        userService.removeUserById(3);
 
-        userDao.getAllUsers();
+        userService.getAllUsers();
 
-        userDao.cleanUsersTable();
+        userService.cleanUsersTable();
 
-        userDao.dropUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,22 +9,27 @@ import java.util.Collection;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private static final String INSERT = "INSERT INTO pz114.users(name, lastname, age) VALUES(?, ?, ?)";
+    private static final String INSERT = "INSERT INTO users(name, lastname, age) VALUES(?, ?, ?)";
+    private static final String CREATE = "CREATE TABLE IF NOT EXISTS users " +
+            "(id bigint primary key not null auto_increment, " +
+            "name varchar(50), " +
+            "lastname varchar(50), " +
+            "age tinyint)";
+    private static final String DROP = "DROP TABLE IF EXISTS users";
+    private static final String REMOVE = "DELETE FROM users WHERE id = ?";
+    private static final String GET = "SELECT * FROM users";
+    private static final String CLEAN = "TRUNCATE TABLE users";
+
+    Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try(Connection connection = Util.getConnection();
-            Statement statement = connection.createStatement())
+        try(PreparedStatement preparedStatement = connection.prepareStatement(CREATE))
         {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS pz114.users" +
-                    "(id bigint primary key not null auto_increment," +
-                    "name varchar(50)," +
-                    "lastname varchar(50)," +
-                    "age tinyint)");
-            System.out.println("Table created!");
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Error: при создании таблицы");
             e.printStackTrace();
@@ -32,11 +37,9 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        try(Connection connection = Util.getConnection();
-            Statement statement = connection.createStatement())
+        try(PreparedStatement preparedStatement = connection.prepareStatement(DROP))
         {
-            statement.executeUpdate("DROP TABLE IF EXISTS pz114.users");
-            System.out.println("Table droped");
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Error: при удалении таблицы");
             e.printStackTrace();
@@ -44,15 +47,13 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Connection connection = Util.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT))
+        try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT))
         {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
 
             preparedStatement.executeUpdate();
-            System.out.println(name + " " + lastName + " добавлен в базу данных");
         } catch (SQLException e) {
             System.out.println("Error: при добавлении пользователя с именем:" + name
             + ", фамилией: " + lastName + ", возрастом: " + age);
@@ -61,11 +62,10 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement())
+        try (PreparedStatement preparedStatement = connection.prepareStatement(REMOVE))
         {
-            statement.executeUpdate("DELETE FROM pz114.users WHERE id = " + id);
-            System.out.println("Пользователь с id(" + id + ") удалён!");
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Error: при удалении пользователя с id = " + id);
             e.printStackTrace();
@@ -75,10 +75,9 @@ public class UserDaoJDBCImpl implements UserDao {
     public List<User> getAllUsers() {
         List <User> users = new ArrayList<>();
 
-        try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement())
+        try (PreparedStatement preparedStatement = connection.prepareStatement(GET))
         {
-            ResultSet res = statement.executeQuery("SELECT * FROM pz114.users");
+            ResultSet res = preparedStatement.executeQuery();
 
             while (res.next()) {
                 User user = new User();
@@ -97,11 +96,9 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try(Connection connection = Util.getConnection();
-            Statement statement = connection.createStatement())
+        try(PreparedStatement preparedStatement = connection.prepareStatement(CLEAN))
         {
-            statement.executeUpdate("TRUNCATE TABLE users");
-            System.out.println("Table clean");
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Error: при очистке таблицы");
             e.printStackTrace();
